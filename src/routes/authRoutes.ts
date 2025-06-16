74e3aad30b7403074cb223db8815e5af62329574
Vedant_Jathar <jatharvedant16@gmail.com>
2025-06-16 09:46:41 +0530

    add test(persist user)

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express'
 import { AuthControllers } from '../controllers/authControllers'
+import { UserService } from '../services/userService'
+import { AppDataSource } from '../config/data-source'
+import { User } from '../entity/User'
 
 const authRouter = Router()
 
-const authController = new AuthControllers()
+const authController = new AuthControllers(
+  new UserService(AppDataSource.getRepository(User)),
+)
 
 authRouter.post('/register', (req: Request, res: Response) =>
   authController.register(req, res),
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
 import app from '../../src/app'
 import request from 'supertest'
+import { AppDataSource } from '../../src/config/data-source'
+import { DataSource } from 'typeorm'
+import { truncateTables } from '../utils'
+import { User } from '../../src/entity/User'
 
 describe('POST /auth/register', () => {
+  let connection: DataSource
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize()
+  })
+
+  beforeEach(async () => {
+    await truncateTables(connection)
+  })
+
+  afterAll(async () => {
+    await connection?.destroy()
+  })
+
   describe('Given all Fields', () => {
     it('Should return status code 201(Created)', async () => {
       // console.log(process.env.DB_NAME);
@@ -39,6 +57,26 @@ describe('POST /auth/register', () => {
       expect(
         (response.headers as Record<string, string>)['content-type'],
       ).toEqual(expect.stringContaining('json'))
+    })
+
+    it('Should persist user in the database', async () => {
+      const userData = {
+        firstName: 'Vedant',
+        lastName: 'Jathar',
+        email: 'jatharvedant16@gmail.com',
+        password: 'ved@123',
+      }
+
+      // Act:
+      await request(app).post('/auth/register').send(userData)
+
+      // Assert:
+      const userRepository = connection.getRepository(User)
+      const users = await userRepository.find()
+      expect(users).toHaveLength(1)
+      expect(users[0].firstName).toBe(userData.firstName)
+      expect(users[0].lastName).toBe(userData.lastName)
+      expect(users[0].email).toBe(userData.email)
     })
   })
 

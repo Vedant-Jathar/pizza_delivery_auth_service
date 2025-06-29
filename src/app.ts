671a90757c59467b8ac104568af6d019fe0e52e4
Vedant_Jathar <jatharvedant16@gmail.com>
2025-06-29 12:02:55 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,16 @@ import authRouter from './routes/authRoutes'
 import cookieParser from 'cookie-parser'
 import tenantRouter from './routes/tenantRoutes'
 import userRouter from './routes/userRoutes'
+import cors from 'cors'
 
 const app = express()
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  }),
+)
+
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())

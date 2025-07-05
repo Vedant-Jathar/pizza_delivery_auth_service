582b61981c45cbf2ae30c418d5cf47a25ffe14b1
Vedant_Jathar <jatharvedant16@gmail.com>
2025-07-05 13:18:58 +0530

    add email validation for update user

--- src/validators/updateUserValidator.ts
+++ src/validators/updateUserValidator.ts
@@ -3,7 +3,7 @@ import z from 'zod'
 export const updateUserSchema = z.object({
   firstName: z.string(),
   lastName: z.string(),
-  email: z.string(),
+  email: z.string().email('Email is not valid'),
   role: z.string(),
   tenantId: z.number(),
 })
